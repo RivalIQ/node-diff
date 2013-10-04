f7b3340ae5510f64d917a1c28bae77d6959f2d6b
Colin Teal <the30yearswar@hotmail.com>
2013-10-03 18:07:37 -0700

    Update Vows, and add tests for custom insert and delete markup generators

--- test/diff-test.js
+++ test/diff-test.js
@@ -13,5 +13,43 @@ vows.describe('Diff Tests').addBatch({
 		'as expected': function(topic) {
 			assert.equal(topic, ' The <del>red </del> brown <ins>spotted </ins> fox <del>jumped </del><ins>leaped </ins> over  the  rolling <del>log.</del><ins>log</ins>');
 		}
-	}
+	},
+    'with custom insert markup': {
+        topic: function() {
+            return diff(
+                'The red brown fox jumped over the rolling log.',
+                'The brown spotted fox leaped over the rolling log',
+                function () { return '$TEST$'; }
+            );
+        },
+        'as expected': function(topic) {
+            assert.equal(topic, ' The <del>red </del> brown $TEST$ fox <del>jumped </del>$TEST$ over  the  rolling <del>log.</del>$TEST$');
+        }
+    },
+    'with custom delete markup': {
+        topic: function() {
+            return diff(
+                'The red brown fox jumped over the rolling log.',
+                'The brown spotted fox leaped over the rolling log',
+                null,
+                function () { return '$TEST$'; }
+            );
+        },
+        'as expected': function(topic) {
+            assert.equal(topic, ' The $TEST$ brown <ins>spotted </ins> fox $TEST$<ins>leaped </ins> over  the  rolling $TEST$<ins>log</ins>');
+        }
+    },
+    'with custom insert and delete markup': {
+        topic: function() {
+            return diff(
+                'The red brown fox jumped over the rolling log.',
+                'The brown spotted fox leaped over the rolling log',
+                function () { return '$INSERT$'; },
+                function () { return '$DELETE$'; }
+            );
+        },
+        'as expected': function(topic) {
+            assert.equal(topic, ' The $DELETE$ brown $INSERT$ fox $DELETE$$INSERT$ over  the  rolling $DELETE$$INSERT$');
+        }
+    }
 }).export(module);
